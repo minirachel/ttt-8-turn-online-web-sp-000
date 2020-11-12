76001d0c20accb797608510dfda301ff5514caf8
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 17:35:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,17 +19,21 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def valid_move?(board,index)
   !position_taken?(board,index) && index.between?(0,8)
 end
+
 
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
 
+
 def move(board, index, value="X")
   board[index] = value
 end
+
 
 def input_to_index(user_input)
   user_input.to_i - 1
