9e1c731236f1a4121ee3456bad4cc8c3ad4d7f94
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 17:32:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,22 +13,15 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
+def display_board
+end
+
 def valid_move?(board,index)
   !position_taken?(board,index) && index.between?(0,8)
 end
 
-
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
-end
-
-def display_board
-end
-
-def valid_move?
-end
-
-def position_taken?
 end
 
 def move
