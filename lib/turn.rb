7952aee84a409e522096e41978b4418edc93b807
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 17:34:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,12 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board,index)
@@ -24,5 +29,11 @@ def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
 
-def move
+def move(board, index, value="X")
+  board[index] = value
+end
+
+#####
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
