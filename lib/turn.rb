5699ff125d1d5882ac3f00bf49d93b12a7b8776d
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 17:28:30 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+#1 build a method composed of many other previously defined methods (helpers)
+#2 Use method return values to control logic??
+#3 use an input validation loop/recursion to create a loop
+
+#turn
+  #ask user for their move by position 1-9
+  #receive user input
+  #convert position to an index
+  #is move valid?
+    #yes- make move and display board
+    #no- ask for different move
