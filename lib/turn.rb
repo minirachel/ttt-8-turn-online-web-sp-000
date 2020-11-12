dee21439ada8de6eecc9b0c1d5241e2a57380290
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 17:31:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,17 @@
     #yes- make move and display board
     #no- ask for different move
 
+board = [" "," "," "," "," "," "," "," "," "]
+
+def valid_move?(board,index)
+  !position_taken?(board,index) && index.between?(0,8)
+end
+
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+    
 def display_board
 end
 
